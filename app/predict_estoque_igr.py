3773c2600fb56bc3fb0983e0b56b6d8e5a21f604
Luan Carvalho <luan.freitas@aquila.com.br>
2023-09-21 18:48:05 -0300

    Adicionando esqueleto da versao beta do modelo de predição

--- app/predict_estoque_igr.py
+++ app/predict_estoque_igr.py
@@ -31,7 +31,7 @@ print("Carregando dados para previsões")
 
 # Prepara os dados para realizar as previsões
 
-zip_file = os.path.join(dataPath, 'estoque_da3.zip')
+zip_file = os.path.join(dataPath, 'base_treino.zip')
 z = zipfile.ZipFile(zip_file)
 
 
@@ -41,53 +41,30 @@ def ler_bases_exportadas(nome_arquivo):
     os.remove(nome_arquivo)
     return df
 
-
-base_conjunta = ler_bases_exportadas('estoque-divida.csv')
-base_parcelas = ler_bases_exportadas('parcelas.csv')
+base_conjunta = ler_bases_exportadas('imovel_mercantil.csv')
 
 print("Inicia transformação das variáveis sobre a dívida")
 print("Processamento das variáveis sobre a dívida")
 
-# Gera as variáveis de tempo
-base_conjunta['data_divida'] = pd.to_datetime(base_conjunta['inscricao_divida'], infer_datetime_format = True)
-base_conjunta['ano_inscricao_da'] = base_conjunta['data_divida'].dt.year
+# # Gera as variáveis de tempo
+# base_conjunta['data_divida'] = pd.to_datetime(base_conjunta['inscricao_divida'], infer_datetime_format = True)
+# base_conjunta['ano_inscricao_da'] = base_conjunta['data_divida'].dt.year
 
 # Seleciona dados sobre a divida
 dados_divida = base_conjunta[
-    ['cda', 'id_contribuinte', 'da_aberto', 'valor_tot', 'vlr_tributo', 'vlr_taxa', 'da_paga', 'arrecadacao_divida',
-     'refis', 'ajuizamento_divida', 'atividade_principal', 'tipo_divida', 'ano_inscricao_da', 'protesto',
-     'ajuizamento', 'base']]
+    ['cda', 'id_contribuinte', 'cpf_cnpj_existe',
+     'da_aberto', 'atividade_principal', 'valor_tot','vlr_pago', 'vlr_tributo', 'vlr_taxa', 'arrecadacao_divida',
+     'ajuizamento_divida', 'ajuizamento', 'protesto', 'competencia_divida', 'refis',
+     'tipo_divida', 'inscricao_divida', 'idade_divida', 'quantidade_reparcelamento', 'situacao'
+     ]]
 dados_divida.dropna(subset=['id_contribuinte'], inplace=True)
 dados_divida['id_contribuinte'] = dados_divida['id_contribuinte'].astype(str)  # persistindo tipo de dados
-
-# Calcula a idade da dívida ativa
-dados_divida['ano_atual'] = date.today().year
-dados_divida['anos_idade_da'] = dados_divida['ano_atual'] - dados_divida['ano_inscricao_da']
-dados_divida = dados_divida.drop(columns=['ano_atual'])
 
 # Renomeia colunas para nome mais adequados e filtra dataframe
 colunas_nome = {
     'valor_tot': 'valor_total_da',
 }
 df_divida_ativa = dados_divida.rename(columns=colunas_nome)
-
-# Obtem os dados de parcelamento das dívidas ativas
-
-base_parcelas.loc[base_parcelas['id_imovel'].isnull(), 'tipo_divida'] = 'mercantil'
-base_parcelas['tipo_divida'] = base_parcelas['tipo_divida'].fillna('imovel')
-base_parcelas['cda'] = base_parcelas['tipo_divida'] + '-' + base_parcelas['cda']
-
-df_parcelas = base_parcelas[['cda', 'quantidade_reparcelamentos']]
-df_parcelas.drop_duplicates(subset='cda', inplace=True)
-df_parcelas.dropna(subset='cda', inplace=True)
-
-df_divida_ativa = pd.merge(
-    left=df_divida_ativa,
-    left_on='cda',
-    right=df_parcelas,
-    right_on='cda',
-    how='left'
-)
 
 print("Processamento das variáveis sobre o contribuinte")
 
@@ -107,14 +84,14 @@ dados_contribuinte = read_s3_files(bucket_name=os.getenv("S3_BUCKET_NAME"), fold
 dados_totais = pd.merge(
     left=df_divida_ativa,
     right=dados_contribuinte,
-    left_on='id_contribuinte',
+    left_on='id_pessoa',
     right_on='id_pessoa',
     how='left')
 
 df = dados_totais[
-    ['cda', 'id_contribuinte', 'da_aberto', 'da_paga', 'arrecadacao_divida', 'tipo_divida', 'valor_total_da',
-     'vlr_tributo', 'vlr_taxa', 'ano_inscricao_da', 'protesto', 'ajuizamento', 'refis', 'ajuizamento_divida', 'cpf_cnpj_existe',
-    'atividade_principal', 'base', 'anos_idade_da', 'quantidade_reparcelamentos', 'qtd_notas_2anos',
+    ['cda', 'id_pessoa', 'da_aberto', 'vlr_pago', 'arrecadacao_divida', 'tipo_divida', 'valor_total_da',
+     'vlr_tributo', 'vlr_taxa', 'inscricao_divida', 'protesto', 'ajuizamento', 'refis', 'ajuizamento_divida', 'cpf_cnpj_existe',
+    'atividade_principal', 'idade_divida', 'quantidade_reparcelamentos', 'qtd_notas_2anos',
      'situacao', 'status_situacao', 'frequencia_da_pessoa', 'historico_pagamento_em_qtd','historico_pagamento_em_valor',
      'class_contribuinte', 'class_contribuinte_nome', 'class_contribuinte_peso']]
 
@@ -128,7 +105,7 @@ df.fillna(0, inplace=True)
 
 print("Preparação do pipeline de previsões")
 # Filtra os dados que precisamos para previsão
-df_feature_store = df[['valor_total_da', 'anos_idade_da', 'quantidade_reparcelamentos',
+df_feature_store = df[['valor_total_da', 'idade_divida', 'quantidade_reparcelamentos',
                        'frequencia_da_pessoa', 'historico_pagamento_em_qtd', 'status_situacao',
                        'historico_pagamento_em_valor', 'class_contribuinte_peso']]
 
@@ -161,7 +138,7 @@ model_predict_igr = abre_modelo("modeloDA-igr-divida-v2.pkl", modelsPath)
 previsoes = model_predict_igr.predict(df_normalizado)
 df['igr'] = previsoes
 df.loc[df['status_situacao'] == 0, 'igr'] = 0
-df.loc[df['anos_idade_da'] >= 15, 'igr'] = 0
+df.loc[df['idade_divida'] >= 15, 'igr'] = 0
 
 print("Início do processo de classificação do rating da dívida parametrizando junto a classificação do contribuinte")
 
@@ -179,14 +156,14 @@ def make_rating_divida(dataframe):
     # dataframe.loc[(dataframe['class_contribuinte'] == 3) & (dataframe['igr'] < 0.1) & (dataframe['igr'] != 0), 'rating_divida'] = 'MEDIA'
 
     # Pagador intermediario
-    dataframe.loc[(dataframe['class_contribuinte'] == 3) & (dataframe['igr'] >= 0.5), 'rating_divida'] = 'ALTA'
-    dataframe.loc[(dataframe['class_contribuinte'] == 3) & (dataframe['igr'] < 0.5) & (dataframe['igr'] >= 0.05), 'rating_divida'] = 'MEDIA'
-    dataframe.loc[(dataframe['class_contribuinte'] == 3) & (dataframe['igr'] < 0.1) & (dataframe['igr'] != 0), 'rating_divida'] = 'BAIXA'
+    dataframe.loc[(dataframe['class_contribuinte'] == 1) & (dataframe['igr'] >= 0.5), 'rating_divida'] = 'ALTA'
+    dataframe.loc[(dataframe['class_contribuinte'] == 1) & (dataframe['igr'] < 0.5) & (dataframe['igr'] >= 0.05), 'rating_divida'] = 'MEDIA'
+    dataframe.loc[(dataframe['class_contribuinte'] == 1) & (dataframe['igr'] < 0.1) & (dataframe['igr'] != 0), 'rating_divida'] = 'BAIXA'
 
     # Bom pagador
-    dataframe.loc[(dataframe['class_contribuinte'] == 1) & (dataframe['igr'] >= 0.5), 'rating_divida'] = 'ALTISSIMA'
-    dataframe.loc[(dataframe['class_contribuinte'] == 1) & (dataframe['igr'] < 0.5) & (dataframe['igr'] >= 0.05), 'rating_divida'] = 'ALTA'
-    dataframe.loc[(dataframe['class_contribuinte'] == 1) & (dataframe['igr'] < 0.05) & (dataframe['igr'] != 0), 'rating_divida'] = 'MEDIA'
+    dataframe.loc[(dataframe['class_contribuinte'] == 3) & (dataframe['igr'] >= 0.5), 'rating_divida'] = 'ALTISSIMA'
+    dataframe.loc[(dataframe['class_contribuinte'] == 3) & (dataframe['igr'] < 0.5) & (dataframe['igr'] >= 0.05), 'rating_divida'] = 'ALTA'
+    dataframe.loc[(dataframe['class_contribuinte'] == 3) & (dataframe['igr'] < 0.05) & (dataframe['igr'] != 0), 'rating_divida'] = 'MEDIA'
 
     # Melhor Pagador
     dataframe.loc[(dataframe['class_contribuinte'] == 4) & (dataframe['igr'] >= 0.3), 'rating_divida'] = 'ALTA'
@@ -212,7 +189,7 @@ def up_s3_files(dataframe, bucket_name, folder_name, file_name):
 up_s3_files(dataframe=df,
             bucket_name=os.getenv("S3_BUCKET_NAME"),
             folder_name=os.getenv("S3_FOLDER_NAME"),
-            file_name='previsoes_igr_status_situacao.csv')
+            file_name='baseline_beta.csv')
 
 print("Upload de dados efetuados no s3")
 print("Processo finalizado")
